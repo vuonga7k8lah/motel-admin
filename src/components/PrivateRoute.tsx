9c1e9a2de52076ee5e265f2cb692ed99148715e5
vuonga7k8lah <vuonga7k8lnc@gmail.com>
2025-09-22 16:00:24 +0700

    add code

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,18 +1,31 @@
-import { ReactNode } from "react";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-import { RootState } from "../redux/store";
+import { ReactNode } from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { RootState } from '../redux/store';
+import { useKeycloak } from '@react-keycloak/web';
 
 interface PrivateRouteProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const PrivateRoute = ({ children }: PrivateRouteProps) => {
-    const isAuthenticated = useSelector(
-        (state: RootState) => state.auth.isAuthenticated
-    );
+  const { keycloak, initialized } = useKeycloak();
 
-    return isAuthenticated ? <>{children}</> : <Navigate to="/login" />;
+  if (!initialized) {
+    return <div>Loading...</div>;
+  }
+  return keycloak.authenticated ? (
+    <>{children}</>
+  ) : (
+    <button type="button" onClick={() => keycloak.login()}>
+      Login
+    </button>
+  );
+  // const isAuthenticated = useSelector(
+  //     (state: RootState) => state.auth.isAuthenticated
+  // );
+
+  // return isAuthenticated ? <>{children}</> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
--- src/main.tsx
+++ src/main.tsx
@@ -10,16 +10,20 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 
 import { router } from './route/routes';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import keycloak from './configs/keycloak';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ToastProvider>
-          <RouterProvider router={router} />
-          <ToastContainer />
-        </ToastProvider>
-      </PersistGate>
-    </Provider>
-  </StrictMode>
+  <ReactKeycloakProvider authClient={keycloak}>
+    <StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ToastProvider>
+            <RouterProvider router={router} />
+            <ToastContainer />
+          </ToastProvider>
+        </PersistGate>
+      </Provider>
+    </StrictMode>
+  </ReactKeycloakProvider>
 );
